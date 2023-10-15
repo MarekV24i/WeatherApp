8ea2ca8da9278b42b3132095e901b972cccddf30
Marek Vyoral <>
2023-10-15 19:43:09 +0200

    search and conditions response entities

---
+++ WeatherApp/Network/Response/CityEntity.swift
@@ -0,0 +1,13 @@
+//
+//  CityEntity.swift
+//  WeatherApp
+//
+//  Created by Marek on 14.10.2023.
+//
+
+struct CityEntity: Codable {
+    
+    var Key: String?
+    var LocalizedName: String?
+    
+}
---
+++ WeatherApp/Network/Response/ConditionsEntity.swift
@@ -0,0 +1,30 @@
+//
+//  ConditionsEntity.swift
+//  WeatherApp
+//
+//  Created by Marek on 14.10.2023.
+//
+
+struct ConditionsEntity: Codable {
+    
+    struct Units: Codable {
+        var Metric: UnitsData?
+    }
+    
+    struct UnitsData: Codable {
+        var Value: Double?
+        var Unit: String?
+    }
+
+    struct Precipitation: Codable {
+        var Precipitation: Units?
+    }
+    
+    var LocalObservationDateTime: String?
+    var WeatherText: String?
+    var Temperature: Units?
+    var ApparentTemperature: Units?
+    var Visibility: Units?
+    var PrecipitationSummary: Precipitation?
+    var MobileLink: String?
+}
