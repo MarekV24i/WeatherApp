09cfcdaac891ad642a1bcce67676760f1b36001c
Marek Vyoral <>
2023-10-15 18:55:28 +0200

    Project setup

---
+++ WeatherApp/WeatherApp.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Marek on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    private let appState = AppState()
+    var body: some Scene {
+        WindowGroup {
+            SearchView().environmentObject(appState).environmentObject(UseCaseContainer(repository: NetworkRepository(), appState: appState))
+        }
+    }
+}
+
