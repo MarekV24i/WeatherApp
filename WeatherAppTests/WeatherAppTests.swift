e8fb44bc675682e006332b4c5a7738a7e609cd55
Marek Vyoral <>
2023-10-23 14:23:17 +0200

    test mapping of conditions

--- WeatherAppTests/WeatherAppTests.swift
+++ WeatherAppTests/WeatherAppTests.swift
@@ -34,13 +34,70 @@ final class WeatherAppTests: XCTestCase {
         
         XCTAssert(cityModel.key == key
                   && cityModel.country == country
-                  && cityModel.name == name, "Network > Domain city mapping failed")
+                  && cityModel.name == name, 
+                  "Network > Domain city mapping failed")
         
         let cityViewModel = CityViewModelMapper.map(model: cityModel)
         
         XCTAssert(cityViewModel.key == key
                   && cityViewModel.country == country
-                  && cityViewModel.name == name, "Domain > Presentation city mapping failed")
+                  && cityViewModel.name == name, 
+                  "Domain > Presentation city mapping failed")
+        
+    }
+    
+    func testConditionsMapping() throws {
+        
+        let time = Date()
+        let text = "sunny"
+        let value: Double = 1.2
+        let unit = "C"
+        let link = "www.google.com"
+        
+        var conditionsEntity = ConditionsEntity()
+        conditionsEntity.LocalObservationDateTime = ISO8601DateFormatter().string(from: time)
+        conditionsEntity.WeatherText = text
+        conditionsEntity.Temperature = ConditionsEntity.Units(Metric: ConditionsEntity.UnitsData(Value: value, Unit: unit))
+        conditionsEntity.ApparentTemperature = ConditionsEntity.Units(Metric: ConditionsEntity.UnitsData(Value: value, Unit: unit))
+        conditionsEntity.Visibility = ConditionsEntity.Units(Metric: ConditionsEntity.UnitsData(Value: value, Unit: unit))
+        conditionsEntity.PrecipitationSummary = ConditionsEntity.Precipitation(Precipitation:
+        ConditionsEntity.Units(Metric: ConditionsEntity.UnitsData(Value: value, Unit: unit)))
+        conditionsEntity.MobileLink = link
+                                                                      
+        let conditionsModel = ConditionsMapper.map(entity: conditionsEntity)
+        
+        XCTAssert(conditionsModel.time?.ISO8601Format() == time.ISO8601Format()
+                  && conditionsModel.text == text
+                  && conditionsModel.temperature?.value == value
+                  && conditionsModel.temperature?.unit == unit
+                  && conditionsModel.feelsLike?.value == value
+                  && conditionsModel.feelsLike?.unit == unit
+                  && conditionsModel.precipitation?.value == value
+                  && conditionsModel.precipitation?.unit == unit
+                  && conditionsModel.visibility?.value == value
+                  && conditionsModel.visibility?.unit == unit
+                  && conditionsModel.link == link,
+                  "Network > Domain conditions mapping failed")
+        
+        let conditionsViewModel = ConditionsViewModelMapper.map(model: conditionsModel)
+        
+        XCTAssert(conditionsViewModel.text == text
+                  && conditionsViewModel.time.contains(String(Calendar.current.component(.hour, from: time)))
+                  && conditionsViewModel.time.contains(String(Calendar.current.component(.minute, from: time)))
+                  && conditionsViewModel.time.contains(String(Calendar.current.component(.hour, from: time)))
+                  && conditionsViewModel.time.contains(String(Calendar.current.component(.day, from: time)))
+                  && conditionsViewModel.time.contains(String(Calendar.current.component(.month, from: time)))
+                  && conditionsViewModel.time.contains(String(Calendar.current.component(.year, from: time)))
+                  && conditionsViewModel.temperature.contains(String(value))
+                  && conditionsViewModel.temperature.contains(String(unit))
+                  && conditionsViewModel.feelsLike.contains(String(value))
+                  && conditionsViewModel.feelsLike.contains(String(unit))
+                  && conditionsViewModel.precipitation.contains(String(value))
+                  && conditionsViewModel.precipitation.contains(String(unit))
+                  && conditionsViewModel.visibility.contains(String(value))
+                  && conditionsViewModel.visibility.contains(String(unit))
+                  && conditionsViewModel.link?.absoluteString == link,
+                  "Domain > Presentation conditions mapping failed")
         
     }
 
