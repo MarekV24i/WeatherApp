79f04168740254f2be860636a36803a353c63cc6
Marek Vyoral <>
2023-10-25 13:14:16 +0200

    add more API keys

--- WeatherApp/Network/Request/Router.swift
+++ WeatherApp/Network/Request/Router.swift
@@ -15,10 +15,12 @@ enum Router {
     var apiKey: URLQueryItem {
         // API keys should be stored safely, normally I use git-secret (but in this demo lets keep it here)
         // NOTE: API is limited to 50 free requests daily, so here are some spare API keys if needed:
+        // xqEe20JnTVNukGoMFYeM6CGNJHYuXky2
+        // ob4AjFsm8BslUKmLGQF4GPppnPs4otSG
         // 0DyjFgu04fdv3eKLkxHEuvA01Or4tkub
         // fqd9KZE3yuxP6JBLF85wnl4lis8qoUCX
         // WFWyMD4unc6dDHh6qAdEUGJ5zD8I8c5a
-        return URLQueryItem(name: "apikey", value: "0DyjFgu04fdv3eKLkxHEuvA01Or4tkub")
+        return URLQueryItem(name: "apikey", value: "xqEe20JnTVNukGoMFYeM6CGNJHYuXky2")
     }
 
     var host: String {
